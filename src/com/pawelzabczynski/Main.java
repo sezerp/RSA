0ddf15d7aec247a74d398fea9ac2e76779c8a19f
sezerp <zabczynskipawe@gmail.com>
2017-04-17 22:19:25 +0100

    add extendedEuclideanAlgorithm method

--- src/com/pawelzabczynski/Main.java
+++ src/com/pawelzabczynski/Main.java
@@ -1,13 +1,23 @@
 package com.pawelzabczynski;
 
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Rsa w = new Rsa(512);
 
-        System.out.println(w.getP());
-        System.out.println(w.getQ());
+//        System.out.println(w.getP());
+//        System.out.println(w.getQ());
+        BigInteger[] x = new BigInteger[3];
+
+        x = w.extendetEuclideanAlgoritm(BigInteger.valueOf(46), BigInteger.valueOf(240));
+
+        for(int i=0; i<3; i++ )
+        {
+            System.out.println(x[i]);
+        }
 
 
     }
--- src/com/pawelzabczynski/Rsa.java
+++ src/com/pawelzabczynski/Rsa.java
@@ -13,6 +13,9 @@ public class Rsa {
     private BigInteger n = new BigInteger("0");
     private BigInteger r = new BigInteger("0");
 
+    private int i;
+    private int j;
+
     private Random rand = new Random();
     private final int nBit = 1024;
 
@@ -66,7 +69,54 @@ public class Rsa {
         return randN;
     }
 
+    private BigInteger getCoprime(BigInteger a, BigInteger b)
+    {
+        BigInteger coprime = new BigInteger("0");
 
+
+        return coprime;
+    }
+
+    public BigInteger[] extendetEuclideanAlgoritm(BigInteger a, BigInteger b)
+    {
+
+        BigInteger[] ret = new BigInteger[3];
+
+
+        BigInteger s0 = BigInteger.ZERO;
+        BigInteger t0 = BigInteger.ONE;
+        BigInteger r0 = b;
+
+        BigInteger s1 = BigInteger.ONE;
+        BigInteger t1 = BigInteger.ZERO;
+        BigInteger r1 = a;
+
+        BigInteger q = BigInteger.valueOf(0);
+
+        BigInteger buff = BigInteger.ZERO;
+
+        while(r0 != BigInteger.ZERO)
+        {
+            q = r1.divide(r0);
+            buff = r0;
+            r0 = r1.subtract(q.multiply(r0));
+            r1 = buff;
+
+            buff = s0;
+            s0 = s1.subtract(q.multiply(s0));
+            s1 = buff;
+
+            buff = t0;
+            t0 = t1.subtract(q.multiply(t0));
+            t1 = buff;
+
+        }
+
+        ret[0] = r1;
+        ret[1] = s0;
+        ret[2] = t1;;
+        return ret;
+    }
 
 
 
@@ -85,6 +135,11 @@ public class Rsa {
     BigInteger getN()
     {
         return this.n;
+    }
+
+    BigInteger getR()
+    {
+        return this.r;
     }
 
 
