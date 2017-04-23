ecb0bb8caa75df290fca5cc97421b9493ced9d3f
sezerp <zabczynskipawe@gmail.com>
2017-04-23 11:49:26 +0100

    first full working class

--- src/com/pawelzabczynski/Main.java
+++ src/com/pawelzabczynski/Main.java
@@ -7,33 +7,26 @@ public class Main {
     public static void main(String[] args) {
 
         Rsa w = new Rsa();
-        BigInteger a;
-        BigInteger p = w.getPrime(6);
+
+        BigInteger p = w.getPrime(1024);
         System.out.println("p: " + p);
-        BigInteger q = w.getPrime(6);
-        System.out.println("q " + q);
+        BigInteger q = w.getPrime(1024);
+        System.out.println("q: " + q);
         BigInteger n = w.calculateN(p, q);
-        System.out.println("n " + n);
+        System.out.println("n: " + n);
         BigInteger r = w.calculateR(p, q);
-        System.out.println("r " + r);
+        System.out.println("r: " + r);
         BigInteger[] e;
         e = w.getCoprime(r);
-        for(int i = 0; i < 4; i++)System.out.println("e[" + i + "] " + e[i]);
+        for(int i = 0; i < 4; i++)System.out.println("e[" + i + "]: " + e[i]);
         BigInteger d = w.getMultipicativeInverse(r, e[2]);
         System.out.println("d: " + d);
 
         BigInteger pub = w.publicFunction(BigInteger.valueOf(10), e[3], n);
-        System.out.println("pub " + pub);
+        System.out.println("pub: " + pub);
         BigInteger priv = w.privateFunction(pub, d, n);
-        System.out.println("priv " + priv);
-
-//        BigInteger[] x;
-//
-//        x = w.extendetEuclideanAlgoritm(BigInteger.valueOf(5), BigInteger.valueOf(1872));
-//        for(int i = 0; i < 3; i++)System.out.println("x[" + i + "] " + x[i]);
-
-
-
+        System.out.println("priv: " + priv);
+        
 
     }
 }
