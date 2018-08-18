ef49a29684f1295e58b82f41dfe4199115e0ca0f
Pawel Zabczynski <pawel.zabczynski@cloudity.pl>
2018-08-18 12:19:16 +0200

    after fix gitignore

---
+++ src/com/pawelzabczynski/Main.java
@@ -0,0 +1,32 @@
+package com.pawelzabczynski;
+
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rsa w = new Rsa();
+
+        BigInteger p = w.getPrime(512);
+        System.out.println("p: " + p);
+        BigInteger q = w.getPrime(512);
+        System.out.println("q: " + q);
+        BigInteger n = w.calculateN(p, q);
+        System.out.println("n: " + n);
+        BigInteger r = w.calculateR(p, q);
+        System.out.println("r: " + r);
+        BigInteger[] e;
+        e = w.getCoprime(r);
+        for(int i = 0; i < 4; i++)System.out.println("e[" + i + "]: " + e[i]);
+        BigInteger d = w.getMultipicativeInverse(r, e[2]);
+        System.out.println("d: " + d);
+
+        BigInteger pub = w.publicFunction(BigInteger.valueOf(127), e[3], n);
+        System.out.println("pub: " + pub);
+        BigInteger priv = w.privateFunction(pub, d, n);
+        System.out.println("priv: " + priv);
+
+
+    }
+}
