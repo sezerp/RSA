14f331187b41f5cd467eb568b821b2df6e60e157
Pawel Zabczynski <pawelzabczynski@pawelzabczynski.com>
2020-02-29 22:23:19 +0100

    change codse style to fit google standards

--- src/com/pawelzabczynski/Main.java
+++ src/com/pawelzabczynski/Main.java
@@ -4,31 +4,30 @@ import java.math.BigInteger;
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Rsa w = new Rsa();
+    Rsa w = new Rsa();
 
-        BigInteger p = w.getPrime(1024);
-        System.out.println("p: " + p);
-        BigInteger q = w.getPrime(1024);
-        System.out.println("q: " + q);
-        BigInteger n = w.calculateN(p, q);
-        System.out.println("n: " + n);
-        BigInteger r = w.calculateR(p, q);
-        System.out.println("r: " + r);
-        BigInteger[] e;
-        e = w.getCoprime(r);
-        for(int i = 0; i < 4; i++)System.out.println("e[" + i + "]: " + e[i]);
-        BigInteger d = w.getMultipicativeInverse(r, e[2]);
-        System.out.println("d: " + d);
+    BigInteger p = w.getPrime(1024);
+    System.out.println("p: " + p);
+    BigInteger q = w.getPrime(1024);
+    System.out.println("q: " + q);
+    BigInteger n = w.calculateN(p, q);
+    System.out.println("n: " + n);
+    BigInteger r = w.calculateR(p, q);
+    System.out.println("r: " + r);
+    BigInteger[] e;
+    e = w.getCoprime(r);
+    for (int i = 0; i < 4; i++) System.out.println("e[" + i + "]: " + e[i]);
+    BigInteger d = w.getMultiplicativeInverse(r, e[2]);
+    System.out.println("d: " + d);
 
-        BigInteger valutToEncrypted = BigInteger.valueOf(122345);
+    BigInteger valueToEncrypted = BigInteger.valueOf(122345);
 
-        BigInteger encryptedValue = w.publicFunction(valutToEncrypted, e[3], n);
-        System.out.println("pub: " + encryptedValue);
-        BigInteger decryptedValue = w.privateFunction(encryptedValue, d, n);
-        System.out.println("priv: " + decryptedValue);
-
-
-    }
+    BigInteger encryptedValue = w.encrypt(valueToEncrypted, e[3], n);
+    System.out.println("Value to encrypted: " + valueToEncrypted);
+    System.out.println("Encrypted value key: " + encryptedValue);
+    BigInteger decryptedValue = w.decrypt(encryptedValue, d, n);
+    System.out.println("Decrypted value: " + decryptedValue);
+  }
 }
--- src/com/pawelzabczynski/Rsa.java
+++ src/com/pawelzabczynski/Rsa.java
@@ -1,139 +1,121 @@
 package com.pawelzabczynski;
 
+import static java.math.BigInteger.probablePrime;
+
 import java.math.BigInteger;
 import java.util.Random;
 
-/**
- * Created by PrZ on 17/04/2017.
- */
+/** Created by PrZ on 17/04/2017. */
 public class Rsa {
 
+  Rsa() {}
 
-    Rsa()
-    {
+  public BigInteger getPrime(int n) {
+    Random rand = new Random();
+    BigInteger prime;
 
+    do {
+      prime = probablePrime(n, rand);
+    } while (!prime.isProbablePrime(100));
+
+    return prime;
+  }
+
+  public BigInteger calculateN(BigInteger p, BigInteger q) {
+    return q.multiply(p);
+  }
+
+  public BigInteger calculateR(BigInteger p, BigInteger q) {
+    BigInteger r = new BigInteger("0");
+    BigInteger x = new BigInteger("0");
+
+    r = p.subtract(BigInteger.valueOf(1));
+    x = q.subtract(BigInteger.valueOf(1));
+
+    return r.multiply(x);
+  }
+
+  public int randOddNumber(int partition) {
+    Random ranndNum = new Random();
+    int randN;
+    do {
+      randN = (partition / 2) + (int) (ranndNum.nextFloat() * (partition / 2));
+    } while ((randN % 10) % 2 != 1);
+
+    return randN;
+  }
+
+  public BigInteger[] extendedEuclideanAlgorithm(BigInteger a, BigInteger b) {
+
+    BigInteger[] ret = new BigInteger[3];
+
+    BigInteger r0 = a;
+    BigInteger s0 = new BigInteger("1");
+    BigInteger t0 = new BigInteger("0");
+
+    BigInteger r1 = b;
+    BigInteger s1 = new BigInteger("0");
+    BigInteger t1 = new BigInteger("1");
+
+    BigInteger q = new BigInteger("0");
+
+    BigInteger buff = new BigInteger("0");
+
+    while (0 != r0.compareTo(BigInteger.ZERO)) {
+      q = r1.divide(r0); //  374, 2, 1
+
+      buff = r0; // buff = 5, buff = 2, buff = 1
+      r0 = r1.subtract(q.multiply(r0)); // 1872 - 5*374 = 2, 5 - 2*2 = 1, 2 - 1*1 = 1
+      r1 = buff; // r1 = 5, r1 = 2, r1 = 1
+
+      buff = s0;
+      s0 = s1.subtract(q.multiply(s0));
+      s1 = buff;
+
+      buff = t0;
+      t0 = t1.subtract(q.multiply(t0));
+      t1 = buff;
     }
 
+    ret[0] = r1;
+    ret[1] = t1;
+    ret[2] = s1;
 
-    public BigInteger getPrime(int n)
-    {
-        Random rand = new Random();
-        BigInteger prime = new BigInteger("0");
+    return ret;
+  }
 
-        do {
-            prime = prime.probablePrime(n, rand);
-        }
-        while(!prime.isProbablePrime(100));
+  public BigInteger[] getCoprime(BigInteger a) {
+    BigInteger[] coprime = new BigInteger[4];
+    BigInteger[] eukEx;
 
-        return prime;
+    do {
+      coprime[3] = BigInteger.valueOf(randOddNumber(10));
+      eukEx = extendedEuclideanAlgorithm(coprime[3], a);
+
+    } while (eukEx[0].compareTo(BigInteger.ONE) != 0);
+
+      System.arraycopy(eukEx, 0, coprime, 0, 3);
+    return coprime;
+  }
+
+  public BigInteger getMultiplicativeInverse(BigInteger r, BigInteger j) {
+    BigInteger mulInv;
+
+    if (j.signum() != 1 && j.signum() != 0) {
+      do {
+        j = j.add(r);
+      } while (j.signum() == -1);
+
     }
+    mulInv = j.mod(r);
+    return mulInv;
+  }
 
-    public BigInteger calculateN(BigInteger p, BigInteger q)
-    {
-        return q.multiply(p);
-    }
+  public BigInteger decrypt(BigInteger x, BigInteger e, BigInteger n) {
+    return x.modPow(e, n);
+  }
 
-    public BigInteger calculateR(BigInteger p, BigInteger q)
-    {
-        BigInteger r = new BigInteger("0");
-        BigInteger x = new BigInteger("0");
-
-        r = p.subtract(BigInteger.valueOf(1));
-        x = q.subtract(BigInteger.valueOf(1));
-
-        return r.multiply(x);
-    }
-
-    public int randOddNumber(int partition)
-    {
-        Random ranndNum = new Random();
-        int randN;
-        do {
-            randN = (partition / 2) + (int)(ranndNum.nextFloat() * (partition / 2 ) );
-        }while( ( randN % 10 ) % 2 != 1);
-
-        return randN;
-    }
-
-    public BigInteger[] extendetEuclideanAlgoritm(BigInteger a, BigInteger b)
-    {
-
-        BigInteger[] ret = new BigInteger[3];
-
-        BigInteger r0 = a;
-        BigInteger s0 = new BigInteger("1");
-        BigInteger t0 = new BigInteger("0");
-
-        BigInteger r1 = b;
-        BigInteger s1 = new BigInteger("0");
-        BigInteger t1 = new BigInteger("1");
-
-
-        BigInteger q = new BigInteger("0");
-
-        BigInteger buff = new BigInteger("0");
-
-        while(0 != r0.compareTo(BigInteger.ZERO))
-        {
-            q = r1.divide(r0); //  374, 2, 1
-
-            buff = r0; // buff = 5, buff = 2, buff = 1
-            r0 = r1.subtract(q.multiply(r0)); // 1872 - 5*374 = 2, 5 - 2*2 = 1, 2 - 1*1 = 1
-            r1 = buff; // r1 = 5, r1 = 2, r1 = 1
-
-            buff = s0;
-            s0 = s1.subtract(q.multiply(s0));
-            s1 = buff;
-
-            buff = t0;
-            t0 = t1.subtract(q.multiply(t0));
-            t1 = buff;
-
-        }
-
-        ret[0] = r1;
-        ret[1] = t1;
-        ret[2] = s1;
-
-        return ret;
-    }
-
-    public BigInteger[] getCoprime(BigInteger a)
-    {
-        BigInteger[] coprime = new BigInteger[4];
-        BigInteger[] eukEx;
-
-        do {
-            coprime[3] = BigInteger.valueOf(randOddNumber(10));
-            eukEx = extendetEuclideanAlgoritm(coprime[3], a);
-
-        }while(eukEx[0].compareTo(BigInteger.ONE) != 0 );
-
-        for(int i=0; i < 3; i++)coprime[i] = eukEx[i];
-        return coprime;
-    }
-
-    public BigInteger getMultipicativeInverse(BigInteger r, BigInteger j)
-    {
-        BigInteger mulInv;
-
-        if(j.signum() == 1 || j.signum() == 0){
-            mulInv = j.mod(r);
-        }
-        else {
-            do{
-                j = j.add(r);
-                System.out.println("j " + j);
-            }while(j.signum() == -1);
-
-            mulInv = j.mod(r);
-        }
-        return mulInv;
-    }
-
-    public BigInteger privateFunction(BigInteger x, BigInteger e, BigInteger n) {return x.modPow(e, n);}
-
-    public BigInteger publicFunction(BigInteger x, BigInteger d, BigInteger n) {return x.modPow(d, n);}
-
-
+  public BigInteger encrypt(BigInteger x, BigInteger d, BigInteger n) {
+    return x.modPow(d, n);
+  }
 }
